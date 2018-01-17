8f639f4fd91c62b185cdd04270c6cc0fbb0339d9
ccmouse <ccmouse@126.com>
2018-01-17 15:25:54 +0800

    queued scheduler
    (cherry picked from commit fa5fe10)

--- crawler/engine/concurrent.go
+++ crawler/engine/concurrent.go
@@ -12,25 +12,29 @@ type ConcurrentEngine struct {
 type Scheduler interface {
 	Submit(Request)
 	ConfigureMasterWorkerChan(chan Request)
+	WorkerReady(chan Request)
+	Run()
 }
 
 func (e *ConcurrentEngine) Run(seeds ...Request) {
-	in := make(chan Request)
 	out := make(chan ParseResult)
-	e.Scheduler.ConfigureMasterWorkerChan(in)
+	e.Scheduler.Run()
 
 	for i := 0; i < e.WorkerCount; i++ {
-		createWorker(in, out)
+		createWorker(out, e.Scheduler)
 	}
 
 	for _, r := range seeds {
 		e.Scheduler.Submit(r)
 	}
 
+	itemCount := 0
 	for {
 		result := <-out
 		for _, item := range result.Items {
-			log.Printf("Got item: %v", item)
+			log.Printf("Got item #%d: %v",
+				itemCount, item)
+			itemCount++
 		}
 
 		for _, request := range result.Requests {
@@ -40,9 +44,11 @@ func (e *ConcurrentEngine) Run(seeds ...Request) {
 }
 
 func createWorker(
-	in chan Request, out chan ParseResult) {
+	out chan ParseResult, s Scheduler) {
+	in := make(chan Request)
 	go func() {
 		for {
+			s.WorkerReady(in)
 			request := <-in
 			result, err := worker(request)
 			if err != nil {
--- crawler/fetcher/fetcher.go
+++ crawler/fetcher/fetcher.go
@@ -8,13 +8,18 @@ import (
 
 	"log"
 
+	"time"
+
 	"golang.org/x/net/html/charset"
 	"golang.org/x/text/encoding"
 	"golang.org/x/text/encoding/unicode"
 	"golang.org/x/text/transform"
 )
 
+var rateLimiter = time.Tick(10 * time.Millisecond)
+
 func Fetch(url string) ([]byte, error) {
+	<-rateLimiter
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
